279c2095082ce54be4bcbf3f5f8df889cdeeb0c5
KevinMoreno <kmoreno@labelti.com>
2020-02-19 17:12:55 -0600

    Correct use of stack allocation

--- HanoiTest.asm
+++ HanoiTest.asm
@@ -4,93 +4,107 @@
 .text
 
 main:
-
-addi $t8,$zero,1 #Set register to 1 for future beq
+addi $t8,$zero,1 	#Set register to 1 for future beq
 lui $1,0x00001001
 ori $1,$1,0x00000000
-add $a0,$0,$1 	#pointer to  tower A
+add $a0,$0,$1 		#pointer to  tower A
 
 lui $1,0x00001001
 ori $1,$1,0x00000020
-add $a1,$0,$1 #pointer to  tower B
+add $a1,$0,$1		 #pointer to  tower B
 
 lui $1,0x00001001
 ori $1,$1,0x00000040
-add $a2,$0,$1 #pointer to  tower C
 
-add $t1,$zero,$a1 #AUX
-add $t2,$zero,$a2 #FIN
+add $a2,$0,$1 		#pointer to  tower C
 
-addi $s0,$zero,4 #number of disks N
-add $s1,$zero,$s0 #i
+add $t1,$zero,$a1 	#AUX
+add $t2,$zero,$a2 	#FIN
 
-for:		 	# for to store all disks in tower A
-beq $s1,$zero,hanoi	#HANOI (N,COM,AUX,FIN)
-sw $s1,0($a0)		#store disk in tower
-add $sp,$sp,-4		#decrease stack pointer
-sw $s1,0($sp)		# save i in stack ponter (n's)
-addi $a0,$a0,4
-sub $s1,$s1,1
-add $t0,$zero,$a0 	#COM
-sub $t0,$t0,4 		#DECREASE COM 
+addi $s0,$zero,4 	#number of disks N
+add $s1,$zero,$s0 	#i
+
+for:		 	# "for" to store all disks in tower A
+beq $s1,$zero,recursive	# jumps to the first call of the hanoi function once all the disks are in tower A
+sw $s1,0($a0)		#store disk in tower A
+addi $a0,$a0,4		#update last position of tower A
+sub $s1,$s1,1		# update i
+add $t0,$zero,$a0 	#COM 
 j for
+
+recursive:
+jal hanoi		#HANOI (N,COM,AUX,FIN) FIRST call to hanoi function
+j exit
 
 hanoi:
 bne $s0,$t8,else	#If(n==1)
-lw $t3,0($t0)   #get disk from tower COM
-sw $zero,0($t0) #remove disk from tower COM
-addi $t2,$t2,4  #increase position at destination tower
-sw $t3,0($t2)   #place disk  to tower FIN
-sub $t0,$t0,4 #update last position of tower
-
-
-#add $sp,$sp,4 #increase stack pointer (n's)
-#lw $s0,0($sp) #load in $s0 reference to stack pointer
+addi $t0, $t0, -4	#moves one byte to the bottom of the origin tower
+lw $t3,0($t0)   	#get disk from tower COM
+sw $zero,0($t0) 	#remove disk from tower COM
+sw $t3,0($t2)  	 	#place disk  to tower FIN
+addi $t2,$t2,4 		#update last position of tower
 jr $ra
 else:
-# swao between aux and fin
-add $t7,$zero,$t2  #temporal = OLD FIN
-add $t2,$zero,$t1  #SWAP NEW FIN = OLD AUX
-add $t1,$zero,$t7  #SWAP NEW AUX= OLD FIN
-sub $s0,$s0,1	#n=n-1;
-# After swap-> t2: aux, t3:com, t1:fin
-jal hanoi   #HANOI(N-1,COM,FIN,AUX)
+#parte stack pointer
+addi $sp,$sp,-8 	#save 2 bytes of memory to store data
+sw $ra,4($sp)   	#store the returning direction in stack
+sw $s0,0($sp)   	#store actual N in stack
+
+# swap between aux and fin
+add $t7,$zero,$t2  	#temporal = OLD FIN
+add $t2,$zero,$t1  	#SWAP NEW FIN = OLD AUX
+add $t1,$zero,$t7  	#SWAP NEW AUX= OLD FIN
+sub $s0,$s0,1		#n=n-1;
+
+# After swap
+jal hanoi  		#recursive call to hanoi
+#aqui tenemos que mover el stack para arriba y agarrar los datos
+lw   $s0, 0($sp)		#load n from the top of the stack	
+lw   $ra, 4($sp)		#load ra direction from the stack	
+addi $sp, $sp, 8		#add 8 to the stack pointer
+
 #swap to get previous values of com,aux,fin
 add $t7,$zero,$t2  #temporal = OLD FIN
 add $t2,$zero,$t1  #SWAP NEW FIN = OLD AUX
 add $t1,$zero,$t7  #SWAP NEW AUX= OLD FIN
-#agregado JD  
+
+#agregado JD  y KM
+addi $t0, $t0, -4	#moves one byte to the bottom of the origin tower
 lw $t3,0($t0)   #get disk from tower COM
 sw $zero,0($t0) #remove disk from tower COM
-addi $t2,$t2,4  #increase position at destination tower
 sw $t3,0($t2)   #place disk  to tower FIN
-sub $t0,$t0,4 #update last position of tower
+addi $t2,$t2,4 #update last position of tower
 
-addi $sp,$sp,4 #increase stack pointer (n's)
-lw $s0,0($sp) #load in $s0 reference to stack pointer
 #fin agregado JD
-# Before swap-> t2: aux, t3:com, t1:fin
 add $t7,$zero,$t0  #temporal = OLD COM
 add $t0,$zero,$t1  #SWAP NEW COM = OLD AUX
 add $t1,$zero,$t7  #SWAP NEW AUX= OLD COM
-
-
-
 #agregado JD
 sub $s0,$s0,1	#n=n-1;
 #After swap -> t2: aux, t3:com, t1:fin
 #Fin agregado JD
-
-#lw $t3,0($t0)   #get disk from tower COM
-#sw $zero,0($t0) #remove disk from tower COM
-#sw $t3,0($t2)   #place disk  to tower FIN
-#sub $t0,$t0,4 #update last position of tower
-#add $t7,$zero,$t2  #temporal = OLD FIN
-#add $t2,$zero,$t1  #SWAP NEW FIN = OLD AUX
-#add $t1,$zero,$t7  #SWAP NEW AUX= OLD FIN
-#add $t7,$zero,$t1 #temporal = OLD AUX
-#add $t1,$zero,$t2  # OLD COM-NEW AUX
-#add $t2,$zero,$t7  #OLD AUX NEW COM
 jal hanoi
+#aqui otra vez hacemos lo del stack, y pasamos del hijo derecho del primer hanoi(2) al print del hanoi(3)
+lw   $s0, 0($sp)		#load n from the top of the stack	
+lw   $ra, 4($sp)		#load ra direction from the stack	
+addi $sp, $sp, 8		#add 8 to the stack pointer
+
+# hacer switch
+add $t7,$zero,$t0  #temporal = OLD COM
+add $t0,$zero,$t1  #SWAP NEW COM = OLD AUX
+add $t1,$zero,$t7  #SWAP NEW AUX= OLD COM
+#hacer switch
+add $t7,$zero,$t2  #temporal = OLD FIN
+add $t2,$zero,$t1  #SWAP NEW FIN = OLD AUX
+add $t1,$zero,$t7  #SWAP NEW AUX= OLD FIN
+
+#mover disco
+addi $t0, $t0, -4	#moves one byte to the bottom of the origin tower
+lw $t3,0($t0)   #get disk from tower COM
+sw $zero,0($t0) #remove disk from tower COM
+sw $t3,0($t2)   #place disk  to tower FIN
+addi $t0,$t0,4 #update last position of tower
+
+jr $ra
 
 j exit
